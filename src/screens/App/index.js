b314881b0fca62d206dac3588c05a0f8755fcee5
Wilka Granjeiro <wilka@MacBook-Pro-de-Wilka.local>
2025-09-20 15:05:33 -0300

    Adiciona arquivos essenciais do expo

---
+++ src/screens/App/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HomeScreen from '../src/screens/HomeScreen';
+
+// Tela inicial exibida pelo Expo Router
+export default function App() {
+  return <HomeScreen />;
+}
