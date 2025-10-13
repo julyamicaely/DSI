6a48a95b8576beba7ab3b5e6b155055c7c87a9db
Juan Gabriel de Oliveira Macedo <jbieeel@gmail.com>
2025-10-12 22:04:47 -0300

    feat: add firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB45tGtVViBLXWS1V6fz0TrP1xr4c3vCKM",
+  authDomain: "lifebeauty.firebaseapp.com",
+  projectId: "lifebeauty",
+  storageBucket: "lifebeauty.firebasestorage.app",
+  messagingSenderId: "1051441526191",
+  appId: "1:1051441526191:web:c9b7eef391308cf15ceb5a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
